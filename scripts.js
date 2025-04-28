430e5d32ea598c1f7f36cac5ee88b97ae41e9867
Ahmed Mustafa <107986169+ahmedmustafam@users.noreply.github.com>
2025-04-28 21:35:29 +0400

    Add basic gameboard function

---
+++ scripts.js
@@ -0,0 +1,16 @@
+const gameBoard = (function () {
+    const board = [];
+    const rows = 3;
+    const columns = 3;
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(0);
+        }
+    };
+
+    return board;
+}) ();
+
+console.log(gameBoard);
